ac6d5bf9084f86d225ea531ff47d3c63c758b49f
James Hunsinger <hunsingeruk@gmail.com>
2023-11-09 16:07:20 -0700

    updated link to to csv file

--- src/config/main.js
+++ src/config/main.js
@@ -1,6 +1,7 @@
 export const config = {
     // credit to reisanar for dataset - @ https://github.com/reisanar/datasets
-    moviesCsvUrl: "https://gist.githubusercontent.com/HunsingerUK/935b2ffb24c4d3542d4bf34f0d98bf69/raw/e1be578514fd50838121d0b509fda97997a2c5b6/movies.csv",
+    
+    moviesCsvUrl: "https://gist.githubusercontent.com/HunsingerUK/935b2ffb24c4d3542d4bf34f0d98bf69/raw/68e8537712c2fd895d20faf4bf83250d225f2129/movies.csv",
     genresCsvUrl: "https://gist.githubusercontent.com/HunsingerUK/2f1e7844425a7f5478c3f113cc30cc00/raw/2e532515d58a12aee46ec9b6b39e7296e4b56c0e/movies-genres.csv",
     originalCsvUrl: "https://raw.githubusercontent.com/reisanar/datasets/master/IMDB_movies.csv"
   };
